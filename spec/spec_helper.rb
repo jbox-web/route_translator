fa0b38a7f64cd0abdde6e54be436c2be05969766
Nicolas Rodriguez <nico@nicoladmin.fr>
2025-10-20 23:03:57 +0200

    Minor change

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,8 +16,19 @@ require File.expand_path('dummy/config/environment.rb', __dir__)
 # Load test gems
 require 'rspec/rails'
 
-# Load our own config
-require_relative 'config_rspec'
+# Configure RSpec
+RSpec.configure do |config|
+  config.order = :random
+  Kernel.srand config.seed
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+
+  # disable monkey patching
+  # see: https://relishapp.com/rspec/rspec-core/v/3-8/docs/configuration/zero-monkey-patching-mode
+  config.disable_monkey_patching!
+end
 
 module TestEngine
   class Engine < ::Rails::Engine
